9563399d538edc85dd8f06cdd9a8bccf5346c371
Клоков Андрей <Klokov.AI@parma.ru>
2024-07-11 21:01:26 +0300

    Add accounts module

---
+++ ts-accounts/src/main/java/ru/klokov/tsaccounts/TsAccountsApplication.java
@@ -0,0 +1,13 @@
+package ru.klokov.tsaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsAccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsAccountsApplication.class, args);
+    }
+
+}
