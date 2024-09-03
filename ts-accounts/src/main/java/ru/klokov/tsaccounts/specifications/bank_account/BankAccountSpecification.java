10c75c608af9d8064f02d6e648d42db9c264d8c1
Клоков Андрей <Klokov.AI@parma.ru>
2024-09-03 17:16:16 +0300

    Корректировка BankAccountSpecification - переход от if-else на switch

--- ts-accounts/src/main/java/ru/klokov/tsaccounts/specifications/bank_account/BankAccountSpecification.java
+++ ts-accounts/src/main/java/ru/klokov/tsaccounts/specifications/bank_account/BankAccountSpecification.java
@@ -16,17 +16,17 @@ public class BankAccountSpecification implements Specification<BankAccountEntity
 
     @Override
     public Predicate toPredicate(Root<BankAccountEntity> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-        if (criteria.getSearchOperation() == SearchOperation.GREATER_THAN) {
-            return criteriaBuilder.greaterThanOrEqualTo(
-                    root.<String> get(criteria.getFieldName()), criteria.getFieldValue().toString());
-        }
-        else if (criteria.getSearchOperation() == SearchOperation.LESS_THAN) {
-            return criteriaBuilder.lessThanOrEqualTo(
-                    root.<String> get(criteria.getFieldName()), criteria.getFieldValue().toString());
-        }
-        else if (criteria.getSearchOperation() == SearchOperation.EQUALITY) {
+        switch (criteria.getSearchOperation()) {
+            case GREATER_THAN:
+                return criteriaBuilder.greaterThanOrEqualTo(
+                        root.get(criteria.getFieldName()), criteria.getFieldValue().toString());
+            case LESS_THAN:
+                return criteriaBuilder.lessThanOrEqualTo(
+                        root.get(criteria.getFieldName()), criteria.getFieldValue().toString());
+            case EQUALITY:
                 return criteriaBuilder.equal(root.get(criteria.getFieldName()), criteria.getFieldValue());
+            default:
+                return null;
         }
-        return null;
     }
 }
