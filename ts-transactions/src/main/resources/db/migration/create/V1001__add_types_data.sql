9f827c989090c23b43c3976cd70b33d6748cc8bb
Клоков Андрей <Klokov.AI@parma.ru>
2025-04-17 20:09:14 +0300

    Корректировка миграций

--- ts-transactions/src/main/resources/db/migration/create/V1001__add_types_data.sql
+++
@@ -1,4 +0,0 @@
-insert into transactions.types (id, "name") values (1, 'Payment');
-insert into transactions.types (id, "name") values (2, 'Deposit');
-insert into transactions.types (id, "name") values (3, 'Cash Withdrawal');
-insert into transactions.types (id, "name") values (4, 'Transfer');
